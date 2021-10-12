ef068cc08d87fcc244d309e2f4db3f506063b8a2
Michael Gambold <7439294+michaelgambold@users.noreply.github.com>
2021-10-12 15:10:29 +1100

    Got lights working.

--- src/io/light.ts
+++ src/io/light.ts
@@ -1,3 +1,5 @@
+import { i2cBegin, i2cSetBaudRate, i2cSetSlaveAddress, i2cWrite } from 'rpio';
+
 export enum LightIndex {
   AnalogInput1 = 0,
   AnalogInput2 = 1,
@@ -26,10 +28,24 @@ export enum LightIndex {
 }
 
 export class LightService {
-  private readonly lights: Light[];
-  private readonly lightCount = 18;
+  readonly lights: Light[];
+
+  private readonly maxBrightness = 128;
+
+  // sn3218 command register addresses
+  private readonly cmdEnableOutput = 0x00;
+  private readonly cmdSetPwmValues = 0x01;
+  private readonly cmdEnableLeds = 0x13;
+  // private readonly cmdEnableLeds7to12 = 0x14;
+  // private readonly cmdEnableLeds13to18 = 0x15;
+  private readonly cmdUpdate = 0x16;
+  private readonly cmdReset = 0x17;
 
   constructor() {
+    i2cBegin();
+    i2cSetSlaveAddress(0x54);
+    i2cSetBaudRate(400_000);
+
     this.lights = [];
 
     // lights are controlled through SN3218 so only have 18 channels
@@ -37,12 +53,61 @@ export class LightService {
       this.lights[i] = new Light(i);
     }
   }
+
+  reset(): void {
+    i2cWrite(Buffer.from([this.cmdReset, 0xff]));
+  }
+
+  disable(): void {
+    i2cWrite(Buffer.from([this.cmdEnableOutput, 0x00]));
+  }
+
+  enable(): void {
+    const status = i2cWrite(Buffer.from([this.cmdEnableOutput, 0x01]));
+    console.log(status);
+  }
+
+  enableLights(enableMask: number): void {
+    // i don't know wtf this is
+    const maskArray = [
+      enableMask & 0x3f,
+      (enableMask >> 6) & 0x3f,
+      (enableMask >> 12) & 0x3f,
+    ];
+
+    i2cWrite(Buffer.from([this.cmdEnableLeds, ...maskArray]));
+    i2cWrite(Buffer.from([this.cmdUpdate, 0xff]));
+  }
+
+  updateLights(): void {
+    const lightData = this.lights.map((light) => {
+      return light.isOn ? this.maxBrightness : 0;
+    });
+
+    i2cWrite(Buffer.from([this.cmdSetPwmValues, ...lightData]));
+    i2cWrite(Buffer.from([this.cmdUpdate, 0xff]));
+  }
 }
 
 export class Light {
+  isOn: boolean;
+
   readonly lightIndex: LightIndex;
 
   constructor(lightIndex: LightIndex) {
     this.lightIndex = lightIndex;
+    this.isOn = false;
+  }
+
+  off(): void {
+    this.isOn = false;
+  }
+
+  on(): void {
+    this.isOn = true;
+  }
+
+  toggle(): void {
+    this.isOn = !this.isOn;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,200 @@
 import { AutomationHat } from './automation-hat';
+import { LightIndex } from './io/light';
 
-const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
+function delay(ms: number) {
+  return new Promise((res) => setTimeout(res, ms));
+}
+
+async function testLights(ah: AutomationHat) {
+  // test all the lights work
+  ah.lights.enable();
+  ah.lights.enableLights(0b111111111111111111);
+
+  // turn on all lights
+  ah.lights.lights[LightIndex.AnalogInput1].on();
+  ah.lights.updateLights();
+  await delay(100);
+
+  ah.lights.lights[LightIndex.AnalogInput2].on();
+  ah.lights.updateLights();
+  await delay(100);
+
+  ah.lights.lights[LightIndex.AnalogInput3].on();
+  ah.lights.updateLights();
+  await delay(100);
+
+  ah.lights.lights[LightIndex.DigitalOutput1].on();
+  ah.lights.updateLights();
+  await delay(100);
+
+  ah.lights.lights[LightIndex.DigitalOutput2].on();
+  ah.lights.updateLights();
+  await delay(100);
+
+  ah.lights.lights[LightIndex.DigitalOutput3].on();
+  ah.lights.updateLights();
+  await delay(100);
+
+  ah.lights.lights[LightIndex.DigitalInput1].on();
+  ah.lights.updateLights();
+  await delay(100);
+
+  ah.lights.lights[LightIndex.DigitalInput2].on();
+  ah.lights.updateLights();
+  await delay(100);
+
+  ah.lights.lights[LightIndex.DigitalInput3].on();
+  ah.lights.updateLights();
+  await delay(100);
+
+  ah.lights.lights[LightIndex.Relay1NO].on();
+  ah.lights.updateLights();
+  await delay(100);
+
+  ah.lights.lights[LightIndex.Relay1NC].on();
+  ah.lights.updateLights();
+  await delay(100);
+
+  ah.lights.lights[LightIndex.Relay2NO].on();
+  ah.lights.updateLights();
+  await delay(100);
+
+  ah.lights.lights[LightIndex.Relay2NC].on();
+  ah.lights.updateLights();
+  await delay(100);
+
+  ah.lights.lights[LightIndex.Relay3NO].on();
+  ah.lights.updateLights();
+  await delay(100);
+
+  ah.lights.lights[LightIndex.Relay3NC].on();
+  ah.lights.updateLights();
+  await delay(100);
+
+  ah.lights.lights[LightIndex.Warn].on();
+  ah.lights.updateLights();
+  await delay(100);
+
+  ah.lights.lights[LightIndex.Comms].on();
+  ah.lights.updateLights();
+  await delay(100);
+
+  ah.lights.lights[LightIndex.Power].on();
+  ah.lights.updateLights();
+  await delay(100);
+
+  // turn off all lights
+  ah.lights.lights[LightIndex.AnalogInput1].off();
+  ah.lights.updateLights();
+  await delay(100);
+
+  ah.lights.lights[LightIndex.AnalogInput2].off();
+  ah.lights.updateLights();
+  await delay(100);
+
+  ah.lights.lights[LightIndex.AnalogInput3].off();
+  ah.lights.updateLights();
+  await delay(100);
+
+  ah.lights.lights[LightIndex.DigitalOutput1].off();
+  ah.lights.updateLights();
+  await delay(100);
+
+  ah.lights.lights[LightIndex.DigitalOutput2].off();
+  ah.lights.updateLights();
+  await delay(100);
+
+  ah.lights.lights[LightIndex.DigitalOutput3].off();
+  ah.lights.updateLights();
+  await delay(100);
+
+  ah.lights.lights[LightIndex.DigitalInput1].off();
+  ah.lights.updateLights();
+  await delay(100);
+
+  ah.lights.lights[LightIndex.DigitalInput2].off();
+  ah.lights.updateLights();
+  await delay(100);
+
+  ah.lights.lights[LightIndex.DigitalInput3].off();
+  ah.lights.updateLights();
+  await delay(100);
+
+  ah.lights.lights[LightIndex.Relay1NO].off();
+  ah.lights.updateLights();
+  await delay(100);
+
+  ah.lights.lights[LightIndex.Relay1NC].off();
+  ah.lights.updateLights();
+  await delay(100);
+
+  ah.lights.lights[LightIndex.Relay2NO].off();
+  ah.lights.updateLights();
+  await delay(100);
+
+  ah.lights.lights[LightIndex.Relay2NC].off();
+  ah.lights.updateLights();
+  await delay(100);
+
+  ah.lights.lights[LightIndex.Relay3NO].off();
+  ah.lights.updateLights();
+  await delay(100);
+
+  ah.lights.lights[LightIndex.Relay3NC].off();
+  ah.lights.updateLights();
+  await delay(100);
+
+  ah.lights.lights[LightIndex.Warn].off();
+  ah.lights.updateLights();
+  await delay(100);
+
+  ah.lights.lights[LightIndex.Comms].off();
+  ah.lights.updateLights();
+  await delay(100);
+
+  ah.lights.lights[LightIndex.Power].off();
+  ah.lights.updateLights();
+  await delay(100);
+
+  // blink twice
+
+  ah.lights.lights.forEach((light) => light.on());
+  ah.lights.updateLights();
+  await delay(200);
+
+  ah.lights.lights.forEach((light) => light.off());
+  ah.lights.updateLights();
+  await delay(200);
+
+  ah.lights.lights.forEach((light) => light.on());
+  ah.lights.updateLights();
+  await delay(200);
+
+  ah.lights.lights.forEach((light) => light.off());
+  ah.lights.updateLights();
+  await delay(200);
+}
+
+async function testRelays(ah: AutomationHat) {
+  // test all the relays work
+  for (let i = 0; i < 2; i++) {
+    ah.relays.one.turnOn();
+    ah.relays.two.turnOn();
+    ah.relays.three.turnOn();
+    await delay(500);
+
+    ah.relays.one.turnOff();
+    ah.relays.two.turnOff();
+    ah.relays.three.turnOff();
+    await delay(500);
+  }
+}
 
 async function main() {
   const ah = new AutomationHat();
 
-  // test all the lights work
-
-  // test all the relays work
-  ah.relays.one.turnOn();
-  ah.relays.two.turnOn();
-  ah.relays.three.turnOn();
-  await delay(500);
-
-  ah.relays.one.turnOff();
-  ah.relays.two.turnOff();
-  ah.relays.three.turnOff();
-  await delay(500);
+  await testLights(ah);
+  await testRelays(ah);
 }
 
 main();
