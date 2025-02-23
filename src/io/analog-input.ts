4e2535d2d751071c794c7c6a598b43ffab3652a3
Michael Gambold <7439294+michaelgambold@users.noreply.github.com>
2025-02-23 15:32:48 +1100

    🚧 Skeleton analog input

--- src/io/analog-input.ts
+++ src/io/analog-input.ts
@@ -5,13 +5,17 @@ export class AnalogInputService {
   readonly input4: AnalogInput;
 
   constructor() {
-    this.input1 = new AnalogInput();
-    this.input2 = new AnalogInput();
-    this.input3 = new AnalogInput();
-    this.input4 = new AnalogInput();
+    this.input1 = new AnalogInput(25.85);
+    this.input2 = new AnalogInput(25.85);
+    this.input3 = new AnalogInput(25.85);
+    this.input4 = new AnalogInput(3.3);
   }
 }
 
 class AnalogInput {
-  // TODO: reverse engineer python library
+  private readonly maxVoltage: number;
+
+  constructor(maxVoltage: number) {
+    this.maxVoltage = maxVoltage;
+  }
 }
